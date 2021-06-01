6f7fa8bbc5e3647703f03136d47b349e2d49a63d
Avishek Das <avishek.das.ayan@gmail.com>
2021-06-01 15:37:40 +0600

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,9 +6,10 @@ from ktrain import text
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report 
 from sklearn.metrics import precision_score,recall_score,f1_score
-import logging
-logger = logging.getLogger()
-logger.disabled = True
+try:
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+except:
+  pass
 
 def printTable(myDict, colList=None):
 
